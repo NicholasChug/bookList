baae4f4b528750eac055d18c670c2b5a62dc2c6b
Nicholas Chug <nicholas.chug@gmail.com>
2019-11-07 12:40:04 -0500

    added localStorage functionality to persist data

--- appES6.js
+++ appES6.js
@@ -53,7 +53,54 @@ class UI {
     }
 }
 
+// Local Storage Class
+class Store {
+    static getBooks() {
+        let books;
+        if (localStorage.getItem('books') === null) {
+            books = []
+        } else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+
+        return books
+    }
+
+    static displayBooks() {
+        const books = Store.getBooks()
+
+        books.forEach(function(book) {
+            const ui = new UI();
+            // Add book to UI
+            ui.addBookToList(book);
+        })
+    }
+
+    static addBook(book) {
+        const books = Store.getBooks()
+
+        books.push(book)
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    static removeBook(isbn) {
+        const books = Store.getBooks()
+
+        books.forEach(function (book, index) {
+            if (book.isbn === isbn) {
+                books.splice(index, 1)
+            }
+        });
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
+
 // Event Listeners
+
+// DOM Load Event
+document.addEventListener('DOMContentLoaded', Store.displayBooks());
 
 // Event Listener for Adding Book
 document.getElementById('book-form').addEventListener('submit', function (e) {
@@ -79,6 +126,9 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
         // Add book to list
         ui.addBookToList(book);
 
+        // Add to LS
+        Store.addBook(book);
+
         // Show success
         ui.showAlert('Book added', 'success');
 
@@ -94,7 +144,11 @@ document.getElementById('book-list').addEventListener('click', function (e) {
 
     const ui = new UI();
 
+    // Delete Book from List
     ui.deleteBook(e.target);
+
+    // Remove from LS
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
     // Show alert for deleted book
     ui.showAlert('Successfully deleted', 'success');
